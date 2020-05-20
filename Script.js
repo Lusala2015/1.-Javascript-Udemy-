5a4d08f39cf900c1e0fd75af161eaeed3bbca356
lusalamark17@gmail.com <lusalamark17@gmail.com>
2020-05-20 21:24:33 +0300

    Udemy JavaScript Course first commit

---
+++ Script.js
@@ -0,0 +1,10 @@
+var firstName = prompt("what is your first name? ");
+var secondName = prompt("what is your second name?");
+var age = prompt('What is your age');
+var fullName = firstName + secondName
+console.log("Your name is  " + fullName );
+console.log("Your age is  " + age);
+var age =prompt("What is your age in years");
+daysLived = age * 365; 
+console.log('You have lived for approximately ' + daysLived +'days' )
+ss
