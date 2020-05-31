f7c0d06000e85f1300845080a9a9237acbbe1b55
Lusala2015 <lusalamark17@gmail.com>
2020-05-31 15:20:36 +0300

    JS Stalker Exercise SOLUTION

--- Script.js
+++ Script.js
@@ -6,5 +6,4 @@ console.log("Your name is  " + fullName );
 console.log("Your age is  " + age);
 var age =prompt("What is your age in years");
 daysLived = age * 365; 
-console.log('You have lived for approximately ' + daysLived +'days' )
-ss
+alert('You have lived for approximately ' + daysLived +'days' )
---
+++ stalkersolution.js
@@ -0,0 +1,7 @@
+var firstName = prompt("what is your first name? ");
+var secondName = prompt("what is your second name?");
+var age = prompt('What is your age');
+var fullName = firstName + secondName
+
+console.log("Your full name is  " + fullName);
+console.log("You are " + age + " years old");
