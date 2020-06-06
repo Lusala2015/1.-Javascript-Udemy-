b489ad86387b8c43639522aa2d4f021d68bf83f9
Lusala2015 <lusalamark17@gmail.com>
2020-06-06 17:13:02 +0300

    check guess

---
+++ Guessing Game Code Along.js
@@ -0,0 +1,13 @@
+// create secrete number
+var secretNumber = 4;
+
+//ask the user for the guess
+var guess = prompt( " Guess a number ");
+alert(guess);
+
+
+if (Number(guess) === secretNumber){
+    alert("YOU GOT IT RIGHT ");
+}
+
+
