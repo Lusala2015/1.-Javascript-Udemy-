b19f4b128e999993ca2522e7168a787d98f6a241
Lusala2015 <lusalamark17@gmail.com>
2020-07-08 10:43:24 +0300

    end of for loop exercises

---
+++ 090 For Loops Quick Exercises.js
@@ -0,0 +1,11 @@
+//exercise 1
+// for (var i = 0; i < 16; i+=8){
+//     console.log(i);
+// }
+
+//exercise 2
+var lettters = "qwertyuiooplkjhgfdsazxcvbnm";
+
+for (i = 1; i < lettters.length; i+=2){
+    console.log(lettters[i]);
+}
