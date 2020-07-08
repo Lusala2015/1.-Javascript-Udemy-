ea2c50a4d00573eede3dc939f84640ce078a8321
Lusala2015 <lusalamark17@gmail.com>
2020-07-08 10:23:35 +0300

    end of intro to for loops

--- 089 Intro to For Loops.js
+++ 089 Intro to For Loops.js
@@ -1,3 +1,9 @@
-for (var count = 0; count <10; count++){
-    console.log("count is " + count);
+// for (var count = 0; count <10; count++){
+//     console.log("count is " + count);
+// }
+
+var str = "hello";
+
+for (var i = 0; i<str.length; i++){
+    console.log(str[i]);
 }
