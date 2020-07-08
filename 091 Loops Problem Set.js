6be8aeaf8da2fe6aa35e4356c02cc8220f005b2a
Lusala2015 <lusalamark17@gmail.com>
2020-07-08 10:48:35 +0300

    4 problem sets for For loop

---
+++ 091 Loops Problem Set.js
@@ -0,0 +1,4 @@
+//print all numbers between -10 and 19
+//print all even numbers between 10 and 40
+//print all odd numbers between 300 and 333
+//print all numbers divisible by 5 AND 3 between 5 and 50
